fa6d4d7c3979fd6c84f5b5770f3bf8a49ed3cd89
Iurii Prytula <iurii.prytula@gmail.com>
2017-08-10 03:16:34 +0300

    added ScrollParallax & MouseParallax

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  //Wait for all
+  //Preloader
   $(function () {
 
     var imgs = [];
@@ -50,13 +50,84 @@ $(document).ready(function () {
         percents++;
       });
     }
-    
-    d.done(function() {
+
+    d.done(function () {
       $('.preloader').css({
-        'display': 'none'
+        'display': 'none',
       });
+      $('.wrapper').css({
+        'display' : 'block'
+      });
+      $('.card').fadeIn(1000);
+      $('.arrow-down-link').fadeIn(3000);
     });
   });
 
-  $('.card').fadeIn(1000);
+  //SoftScroll
+  $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function (e) {
+      e.preventDefault();
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  //ScrollParallax
+  $(window).scroll(function () {
+    var
+      wScroll = $(window).scrollTop();
+
+    function scrollParallax(block, strafeAmount) {
+      var 
+        strafe = -strafeAmount + '%',
+        transformString = 'translate3d(0,' + strafe + ', 0)';
+
+        block.css({
+          'transform': transformString
+        });
+    }
+
+    scrollParallax($('.hero__bg'), -wScroll/40);
+  });
+
+  //MouseParallax
+  var  layer = $('.parallax').find('.parallax__layer');
+
+  layer.map(function(key, value) {
+        var bottomPos = (window.innerHeight / 2) * (key / 100);
+
+        console.log(value);
+        $(value).css({
+          'transform' : 'translate3d(0, 0, 0)'
+        });
+      });
+
+  $(window).mousemove(function (e) { 
+    var
+      mouseX = e.pageX,
+      mouseY = e.pageY,
+      w = (window.innerWidth / 2) - mouseX,
+      h = window.innerHeight / 2 - mouseY;
+     
+      
+      layer.map(function(key, value) {
+        var 
+          bottomPos = (window.innerHeight / 2) * (key / 100),
+          widthPos = w * ((key) / 100),
+          heightPos = h * (key / 100);
+        console.log(value);
+        $(value).css({
+          'bottom' : '-' + bottomPos + 'px',
+          'transform' : 'translate3d(' + widthPos + 'px, ' + heightPos + 'px, 0)'
+        });
+      });
+  });
 });
